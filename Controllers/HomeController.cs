edf19edae031586bc943d9a52c07ab74a8e757e2
k s <klaus.stolz@web.de>
2021-02-22 13:36:24 +0100

    Änderung

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -9,11 +9,6 @@ using System.Threading.Tasks;
 
 namespace PiratenVerein.Controllers {
     public class HomeController : Controller {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController ( ILogger<HomeController> logger ) {
-            _logger = logger;
-        }
 
         public IActionResult Index ( ) {
             return View ( );
@@ -21,11 +16,6 @@ namespace PiratenVerein.Controllers {
 
         public IActionResult Privacy ( ) {
             return View ( );
-        }
-
-        [ResponseCache ( Duration = 0 , Location = ResponseCacheLocation.None , NoStore = true )]
-        public IActionResult Error ( ) {
-            return View ( new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier } );
         }
     }
 }
